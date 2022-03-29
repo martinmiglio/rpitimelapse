c814e8bd82a2f862499b3983ff0eb1487f83a433
Martin Miglio <marmig0404@gmail.com>
2022-03-29 03:13:54 -0400

    fixed removing ns from file name

--- camera_service.py
+++ camera_service.py
@@ -7,6 +7,6 @@ import picamera
 WAIT_TIME = 60
 
 with picamera.PiCamera() as camera:
-    for filename in camera.capture_continuous(f'images/img{time.time().split(".")[0]}.jpg'):
+    for filename in camera.capture_continuous(f'images/img{int(time.time())}.jpg'):
         print(f"took a photo at {time.time()}")
         sleep(WAIT_TIME)
