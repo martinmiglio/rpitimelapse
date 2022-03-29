29d3ab55b3a36c25ee63c609736a44ae9c505e10
Martin Miglio <marmig0404@gmail.com>
2022-03-29 01:28:48 -0400

    updated resolution to match camera

--- camera_service.py
+++ camera_service.py
@@ -7,6 +7,5 @@ import picamera
 WAIT_TIME = 60
 
 with picamera.PiCamera() as camera:
-    camera.resolution = (1024, 768)
     for filename in camera.capture_continuous(f'images/img{time.time()}.jpg'):
         sleep(WAIT_TIME)
