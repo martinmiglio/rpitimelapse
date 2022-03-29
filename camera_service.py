195eb8ac11f89682a6eb4eef05321ec7073a08de
Martin Miglio <marmig0404@gmail.com>
2022-03-29 03:31:38 -0400

    decreased image size to reduce render times

--- camera_service.py
+++ camera_service.py
@@ -7,6 +7,7 @@ import picamera
 WAIT_TIME = 60
 
 with picamera.PiCamera() as camera:
+    camera.resolution = (852, 480)
     while True:
         camera.capture(f'images/img{int(time.time())}.jpg')
         print(f"took a photo at {int(time.time())}")
