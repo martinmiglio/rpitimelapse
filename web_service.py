1ca3a7b5b7c09aad9a8fd12bd780cc61909b3d23
Martin Miglio <marmig0404@gmail.com>
2022-03-30 19:23:49 -0400

    reverted back to production port

--- web_service.py
+++ web_service.py
@@ -84,4 +84,4 @@ def generate_video(image_names):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=8000)
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=80)
