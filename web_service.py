916fe1f05830cd8c064451ef69e0e03b52403562
Martin Miglio <marmig0404@gmail.com>
2022-04-24 02:00:36 -0400

    switched to production port

--- web_service.py
+++ web_service.py
@@ -135,4 +135,4 @@ def set_light():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=8000)
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=80)
