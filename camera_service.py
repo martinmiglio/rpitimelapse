0c9222a665568c33f1b0ba5167b961a81cd51b81
Martin Miglio <marmig0404@gmail.com>
2022-08-05 17:33:15 -0400

    handle case when not on raspberry pi

--- camera_service.py
+++ camera_service.py
@@ -1,13 +1,18 @@
-import time
-from time import sleep
 
-import picamera
+from sys import exit
+from time import sleep, time
 
-WAIT_TIME = 60
+try:
+    import picamera
+except ModuleNotFoundError:
+    print("not running on raspberry pi!")
+    exit()
+else:
+    WAIT_TIME = 60
 
-with picamera.PiCamera() as camera:
-    camera.resolution = (1280, 720)
-    while True:
-        camera.capture(f'images/img{int(time.time())}.jpg')
-        print(f"took a photo at {int(time.time())}")
-        sleep(WAIT_TIME)
+    with picamera.PiCamera() as camera:
+        camera.resolution = (1280, 720)
+        while True:
+            camera.capture(f'images/img{int(time())}.jpg')
+            print(f"took a photo at {int(time())}")
+            sleep(WAIT_TIME)
